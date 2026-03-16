ec0b329ab591205d6167ea6a71c62b00cef0addb
radewoosh <yadavvikas507@gmail.com>
2026-03-16 15:31:03 +0530

    fixed the syntax error

--- hrms_backend/hrms_backend/settings.py
+++ hrms_backend/hrms_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!l8octh0*lui=c4d8o!g%nkxl%b-#=2s+(91!#l56_en2wwiw6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
